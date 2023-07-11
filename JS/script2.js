100d4b5a84af9dd9896fde95da390696abcf6f83
bharatchandran <bharatchandrankr@gmail.com>
2023-07-11 08:59:00 +0530

    declared the checkIfCanMove function -next debugging

--- JS/script2.js
+++ JS/script2.js
@@ -70,34 +70,47 @@ function getRedIdx () {
 }
 
 
-function checkIfCanMOve (piece) {
+function checkIfCanMOve (i, j) {
     let moveLeftDiag;
     let moveRightDiag;
-    for ( let i = 0; i < 7; i++ ) {
-        for (let j = 0; j < 7 ; j++) {
+    console.log(board)
+    
             if(board[i][j] > 7 && board[i][j] !== null ) {
+    
+
                 if(i === 0){
                     moveLeftDiag = false;
                     moveRightDiag = false;
                     return {
                             "moveRightDiag" : moveRightDiag,
-                            "moveLeftDiag" : moveLeftDiag
+                            "moveLeftDiag" : moveLeftDiag,
+                            idx : `r${i}c${j}`
+
                     }
                 } else if (j <= 0) {
+                    console.log(board[i - 1][j + 1])
                     if (board[i - 1][j + 1] === null ){
+                    console.log("Hello")
+
                         moveLeftDiag = false;
                         moveRightDiag = true;
                         return{
                             "moveRightDiag" : moveRightDiag,
-                            "moveLeftDiag" : moveLeftDiag
+                            "moveLeftDiag" : moveLeftDiag,
+                            idx : `r${i}c${j}`
                         }
                         
                     } else {
+                    console.log("Hello")
+                    console.log(board[i - 1][j + 1])
+
+
                         moveLeftDiag = false;
                         moveRightDiag = false;
                         return{
                             "moveRightDiag" : moveRightDiag,
-                            "moveLeftDiag" : moveLeftDiag
+                            "moveLeftDiag" : moveLeftDiag,
+                            idx : `r${i}c${j}`
                         }
                     
                     }
@@ -107,7 +120,8 @@ function checkIfCanMOve (piece) {
                         moveRightDiag = false;
                         return {
                             "moveLeftDiag" : moveLeftDiag,
-                            "moveRightDiag" : moveRightDiag
+                            "moveRightDiag" : moveRightDiag,
+                            idx : `r${i}c${j}`
                         }
                         
                     } else {
@@ -115,7 +129,8 @@ function checkIfCanMOve (piece) {
                         moveRightDiag = false;
                         return {
                             "moveLeftDiag" : moveLeftDiag,
-                            "moveRightDiag" : moveRightDiag
+                            "moveRightDiag" : moveRightDiag,
+                            idx : `r${i}c${j}`
                         }
                     }
                 } else {
@@ -131,12 +146,13 @@ function checkIfCanMOve (piece) {
                     }
                     return {
                         'moveRightDiag' : moveRightDiag,
-                        'moveLeftDiag' : moveLeftDiag
+                        'moveLeftDiag' : moveLeftDiag,
+                        idx : `r${i}c${j}`
                     }
                 }
             }
-        }
-    }
+        
+    
 }
 
 
@@ -166,7 +182,13 @@ function pieceSelection () {
         let redPieceIdx = getRedIdx()
         console.log(redPieceIdx);
         redPieceIdx.forEach( (red) => {
-            console.log(red)
+            red = red.split("")
+            let checkMove = checkIfCanMOve(red[1],red[3]);
+            if (checkMove.moveLeftDiag === true || checkMove.moveRightDiag === true){
+                document.querySelector(`#r${red[1]}c${red[3]} > div`).addEventListener("click",handleMove)
+            } else return
+            console.log(checkMove)
+            // console.log(red.split(""))
         })
     }
 }
